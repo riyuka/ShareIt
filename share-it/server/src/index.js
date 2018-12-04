64d6cc64ef85e9f709223de9c286017b2c4de8df
sarahg813 <goon.sarah@gmail.com>
2018-12-04 00:03:23 -0800

    added styling

--- share-it/server/src/index.js
+++ share-it/server/src/index.js
@@ -1,29 +1,30 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Subscription = require('./resolvers/Subscription')
-const Feed = require('./resolvers/Feed')
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
+const Feed = require("./resolvers/Feed");
 
 const resolvers = {
   Query,
   Mutation,
   Subscription,
-  Feed,
-}
+  Feed
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://us1.prisma.sh/yukakagura-caa46c/share-it/demo',
-      secret: 'mysecret111111',
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://us1.prisma.sh/sarah-g-936eeb/share-it/demo",
+      // https://us1.prisma.sh/yukakagura-caa46c/share-it/demo
+      secret: "mysecret111111",
       debug: true
-    }),
-  }),
-})
+    })
+  })
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log("Server is running on http://localhost:4000"));
